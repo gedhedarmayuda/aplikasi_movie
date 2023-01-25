70b2208dfe1f87dae08aac15b2051370ac25e508
Gede Darma Yuda <gedheyuda398@gmail.com>
2023-01-25 14:08:04 +0700

    final commit

--- lib/component/http_helper.dart
+++ lib/component/http_helper.dart
@@ -1,6 +1,40 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'movie.dart';
 
-class HttpHelper{
-  
+class HttpHelper {
+  final String urlKey = '8b2201d73dba2456434a4dd5f12721e8';
+  final String urlBase = 'https://api.themoviedb.org/3/movie/';
+  final String urlUpcoming = '/upcoming?';
+  final String urlLanguage = '&language=en-US';
+  final String urlApikeys = 'api_key=';
+  final String urlSearchBase =
+      'https://api.themoviedb.org/3/search/movie?api_key=8b2201d73dba2456434a4dd5f12721e8&query=';
+
+  Future<List> getUpcoming() async {
+    final String upcoming =urlBase + urlUpcoming + urlApikeys + urlKey + urlLanguage;
+    http.Response result = await http.get(Uri.parse(upcoming));
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      return urlKey.codeUnits;
+    }
+  }
+
+  Future<List> findMovies(String title) async {
+    final String query = urlSearchBase + title;
+    http.Response result = await http.get(Uri.parse(query));
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(result.body);
+      final movieMap = jsonResponse['results'];
+      List movies = movieMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      return urlKey.codeUnits;
+    }
+  }
 }
---
+++ lib/component/movie_list.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'http_helper.dart';
+import 'movie_detail.dart';
+
+class MovieList extends StatefulWidget {
+  const MovieList({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MovieListState createState() => _MovieListState();
+}
+
+class _MovieListState extends State<MovieList> {
+  String result = 'value';
+  late HttpHelper helper;
+  int moviesCount = 0;
+  late List movies;
+  final String iconBase = 'https://image.tmdb.org/t/p/w92/';
+  final String defaultImage =
+      'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
+  Icon visibleIcon = const Icon(Icons.search);
+  Widget searchBar = const Text('Movies');
+
+  @override
+  void initState() {
+    helper = HttpHelper();
+    initialize();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    NetworkImage image;
+    return Scaffold(
+        appBar: AppBar(title: searchBar, actions: <Widget>[
+          IconButton(
+            icon: visibleIcon,
+            onPressed: () {
+              setState(() {
+                if (visibleIcon.icon == Icons.search) {
+                  visibleIcon = const Icon(Icons.cancel);
+                  searchBar = TextField(
+                    textInputAction: TextInputAction.search,
+                    onSubmitted: (String text) {
+                      search(text);
+                    },
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  );
+                } else {
+                  setState(() {
+                    visibleIcon = const Icon(Icons.search);
+                    searchBar = const Text('Movies');
+                  });
+                }
+              });
+            },
+          ),
+        ]),
+        body: ListView.builder(
+            // ignore: unnecessary_null_comparison
+            itemCount: (moviesCount == null) ? 0 : moviesCount,
+            itemBuilder: (BuildContext context, int position) {
+              if (movies[position].posterPath != null) {
+                image = NetworkImage(iconBase + movies[position].posterPath);
+              } else {
+                image = NetworkImage(defaultImage);
+              }
+              return Card(
+                  color: Colors.white,
+                  elevation: 2.0,
+                  child: ListTile(
+                    onTap: () {
+                      MaterialPageRoute route = MaterialPageRoute(
+                          builder: (_) => MovieDetail(movies[position]));
+                      Navigator.push(context, route);
+                    },
+                    leading: CircleAvatar(
+                      backgroundImage: image,
+                    ),
+                    title: Text(movies[position].title),
+                    // ignore: prefer_interpolation_to_compose_strings
+                    subtitle: Text(
+                        // ignore: prefer_interpolation_to_compose_strings
+                        '${'Released: ' + movies[position].releaseDate} - Vote: ${movies[position].voteAverage}'),
+                  ));
+            }));
+  }
+
+  Future search(text) async {
+    movies = await helper.findMovies(text);
+    setState(() {
+      moviesCount = movies.length;
+      movies = movies;
+    });
+  }
+
+  Future initialize() async {
+    movies = [];
+    movies = await helper.getUpcoming();
+    setState(() {
+      moviesCount = movies.length;
+      movies = movies;
+    });
+  }
+}
